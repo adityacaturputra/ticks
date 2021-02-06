d674f659982ca4c20f246059fdd9950320646e5f
Aditya Catur Putra <adityacaturputra25@gmail.com>
2021-02-06 16:50:03 +0700

    UserController and UserList

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,5 @@ Auth::routes();
 
 // Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/dashboard', 'dashboard\DashboardController@index');
+//Users
+Route::get('/dashboard/users', 'Dashboard\UserController@index');
