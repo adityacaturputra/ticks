898a00e284e37e4aa5b6e1a38175bf7b0aed5c03
Aditya Catur Putra <adityacaturputra25@gmail.com>
2021-02-08 16:25:12 +0700

    middleware auth group on routes

--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,12 @@ Auth::routes();
 
 // Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/dashboard', 'Dashboard\DashboardController@index');
-//Users
-Route::get('/dashboard/users', 'Dashboard\UserController@index')->name('dashboard.users');
-Route::get('/dashboard/users/{id}', 'Dashboard\UserController@edit')->name('dashboard.users.edit');
-Route::put('/dashboard/users/{id}', 'Dashboard\UserController@update')->name('dashboard.users.update');
-Route::delete('/dashboard/users/{id}', 'Dashboard\UserController@destroy')->name('dashboard.users.delete');
+
+Route::middleware('auth')->group(function(){
+    //Users
+    Route::middleware('auth')->get('/dashboard/users', 'Dashboard\UserController@index')->name('dashboard.users');
+    Route::get('/dashboard/users/{id}', 'Dashboard\UserController@edit')->name('dashboard.users.edit');
+    Route::put('/dashboard/users/{id}', 'Dashboard\UserController@update')->name('dashboard.users.update');
+    Route::delete('/dashboard/users/{id}', 'Dashboard\UserController@destroy')->name('dashboard.users.delete');
+});
 
