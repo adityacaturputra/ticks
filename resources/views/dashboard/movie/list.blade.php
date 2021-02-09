79633415ce696a50af1b5014ed79bfaeb7c511c5
Aditya Catur Putra <adityacaturputra25@gmail.com>
2021-02-09 22:07:09 +0700

    update movie list belum ada data

--- resources/views/dashboard/movie/list.blade.php
+++ resources/views/dashboard/movie/list.blade.php
@@ -20,28 +20,31 @@
             </div>
         </div>
         <div class="card-body p-0">
-            <table class="table table-borderless table-striped table-hover">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Title</th>
-                        <th>Thumbnail</th>
-                        <th>&nbsp;</th>
-                    </tr>
-                </thead>
-                <tbody>
-                @foreach ($movies as $movie)
-                    <tr>
-                        <th scope="row">{{ $loop->iteration + ($movies->currentPage() - 1) * $movies->perPage() }}</th>
-                        <td>{{ $movie->title }}</td>
-                        <td>{{ $movie->thumbnail }}</td>
-                        <td><a title="edit" href="{{ route('dashboard.movies.edit' , ['id' => $movie->id]) }}" class="btn btn-success btn-sm"><i class="fas fa-pen"></i></a></td>
-                    </tr>
+            @if ($movies->total())
+                <table class="table table-borderless table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Title</th>
+                            <th>Thumbnail</th>
+                            <th>&nbsp;</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @foreach ($movies as $movie)
+                        <tr>
+                            <th scope="row">{{ $loop->iteration + ($movies->currentPage() - 1) * $movies->perPage() }}</th>
+                            <td>{{ $movie->title }}</td>
+                            <td>{{ $movie->thumbnail }}</td>
+                            <td><a title="edit" href="{{ route('dashboard.movies.edit' , ['id' => $movie->id]) }}" class="btn btn-success btn-sm"><i class="fas fa-pen"></i></a></td>
+                        </tr>
                     @endforeach
-                </tbody>
-            </table>
-
-            {{ $movies->appends($request)->links() }}
+                    </tbody>
+                </table>
+                {{ $movies->appends($request)->links() }}
+            @else
+                <h4 class="text-center p-3">Belum ada data movies</h4>
+            @endif
         </div>
     </div>
 @endsection
