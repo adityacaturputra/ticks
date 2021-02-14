ba63ff7340a3d0908355f908aad671c9ca13f8cd
Aditya Catur Putra <adityacaturputra25@gmail.com>
2021-02-14 16:46:52 +0700

    require theater models

--- app/Http/Controllers/dashboard/TheaterController.php
+++ app/Http/Controllers/dashboard/TheaterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Models\Theater;
 use Illuminate\Http\Request;
 
 class TheaterController extends Controller
