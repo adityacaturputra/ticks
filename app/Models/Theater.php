678770a55b21f3a91595152b0bd20f7e6e3422d9
Aditya Catur Putra <adityacaturputra25@gmail.com>
2021-02-15 11:51:36 +0700

    manual theater models connnect to table

--- app/Models/Theater.php
+++ app/Models/Theater.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Softdeletes;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Theater extends Model
 {
-    use Softdeletes;
+    use SoftDeletes;
+
+    protected $table = 'theaters';
+
 }
